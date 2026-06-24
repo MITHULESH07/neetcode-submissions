c66b12ac65aab2f00bdc6c41f203897fa606c8aa
MITHULESH S <mithulesh.sj@gmail.com>
2026-06-24 15:34:51 +0530

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int m = t.size(),n=s.size();
+        int l=0,r,len=INT_MAX;
+        string ans="";
+        unordered_map<char,pair<int,int>>mp;
+        for(char c:t){
+            mp[c].second++;
+        }
+        for(r=0;r<n;r++){
+            mp[s[r]].first++;
+            while(r-l+1>=m){
+                bool check=true;
+                for(auto b:mp){
+                    if(b.second.second!=0 && b.second.first<b.second.second){
+                        check=false;
+                        break;
+                    }
+                }
+                if(check){
+                    if((r-l+1)<len){
+                        ans= s.substr(l,(r-l+1));
+                        len = r-l+1;
+                    }
+                    mp[s[l]].first--;
+                    if(mp[s[l]].first<0)
+                        mp[s[l]].first=0;
+                    l++;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+        // while(r-l+1>=m){
+        //     cout<<s.substr(l,(r-l+1))<<endl;
+        //     bool check=true;
+        //         for(auto b:mp){
+        //             if(b.second.second!=0 && b.second.first<b.second.second){
+        //                 check=false;
+        //                 break;
+        //             }
+        //         }
+        //     if(check){
+        //             if((r-l+1)<len){
+        //                 ans= s.substr(l,(r-l+1));
+        //                 len = r-l+1;
+        //             }
+        //             mp[s[l]].first--;
+        //             if(mp[s[l]].first<0)
+        //                 mp[s[l]].first=0;
+        //             l++;
+        //         }
+        //         else{
+        //             break;
+        //         }
+        // }
+        
+        return ans;
+    }
+};
